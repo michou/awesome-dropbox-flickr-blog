d035867fabb4d4eb53ca194aee72d10ca13fdeaf
Mihai Balan <mihai.balan@gmail.com>
2017-09-13 23:02:02 +0300

    Initial attempt at making it work on Digital Ocean

--- wsgi/envutils.py
+++ wsgi/envutils.py
@@ -1,8 +1,18 @@
 import os
 
+PRODUCTION_MODE = True
+OVERRIDES = {
+    'OPENSHIFT_REPO_DIR': '.',
+    # HACK-HACK-HACK: These should be actually taken from a config file or something
+    # Hardcoding these values until I write some proper code to encapsulate these settings
+    'OPENSHIFT_DATA_DIR': '/var/www/blog'
+}
 
 def set_local_or_prod(env_key, remote_path, local_path):
-    if env_key in os.environ:
-        return os.path.realpath(os.path.join(os.environ[env_key], remote_path))
+    if PRODUCTION_MODE:
+        if env_key in os.environ:
+            return os.path.realpath(os.path.join(os.environ[env_key], remote_path))
+        else:
+            return os.path.realpath(os.path.join(OVERRIDES[env_key], remote_path))
     else:
         return os.path.realpath(local_path)
--- wsgi/local.py
+++ wsgi/local.py
@@ -4,7 +4,9 @@
 #  ALWAYS RUN FROM PROJECT ROOT, NOT FROM wsgi/ OR FARTHER AWAY
 ################################################################################
 from blog import app as application
+import envutils
 from bottle import run
 
 if __name__ == '__main__':
+    envutils.PRODUCTION_MODE = False
     run(application, host='0.0.0.0', port=8080, reloader=True, debug=True)
